73e7d6fa2a89776c43fe32275989e46c80ee64f4
Michael Courtier <michael.courtier@gmail.com>
2026-05-30 21:54:02 +0200

    feat: AppFooter

---
+++ app/components/AppFooter.vue
@@ -0,0 +1,27 @@
+<template>
+  <UFooter
+    :ui="{
+      // root: 'border-t border-neutral-700',
+      left: 'hidden',
+      right: 'hidden',
+    }"
+  >
+    <template #left>
+      <p class="text-muted text-sm">
+        Copyright © {{ new Date().getFullYear() }} Objectif Sport
+      </p>
+    </template>
+
+    <UNavigationMenu
+      :items="footerLegal"
+      variant="link"
+      :ui="{
+        link: 'text-xs text-neutral-400 transition-colors hover:text-neutral-300',
+      }"
+    />
+  </UFooter>
+</template>
+
+<script setup lang="ts">
+const { footerLegal } = useSiteNavigation()
+</script>
--- app/components/AppShell.vue
+++ app/components/AppShell.vue
@@ -10,33 +10,6 @@
       </main>
     </div>
 
-    <UFooter
-      :ui="{
-        // root: 'border-t border-neutral-700',
-        left: 'hidden',
-        right: 'hidden',
-      }"
-    >
-      <template #left>
-        <p class="text-muted text-sm">
-          Copyright © {{ new Date().getFullYear() }} Objectif Sport
-        </p>
-      </template>
-
-      <UNavigationMenu
-        :items="footerLegal"
-        variant="link"
-        :ui="{
-          link: 'text-xs text-neutral-400 transition-colors hover:text-neutral-300',
-        }"
-      />
-    </UFooter>
+    <AppFooter />
   </div>
 </template>
-
-<script setup lang="ts">
-const { footerLegal } = useSiteNavigation()
-const {
-  public: { copyrightYear },
-} = useRuntimeConfig()
-</script>
